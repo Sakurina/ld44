2801c7141c10c5cbcdb891e909f336d2f50eee90
Yanik Magnan <kirbykirbykirby@gmail.com>
2019-04-28 16:38:00 -0400

    fixed packaging script

--- package.bat
+++ package.bat
@@ -1,4 +1,5 @@
-powershell Compress-Archive . ld44.love
-move ld44.love pkg\ld44.love
+powershell Compress-Archive -Path .\* ld44.zip
+move ld44.zip pkg\ld44.love
 copy /b pkg\win32\love.exe+pkg\ld44.love pkg\win32\ld44.exe
 copy /b pkg\win64\love.exe+pkg\ld44.love pkg\win64\ld44.exe
+.\pkg\win64\ld44.exe
